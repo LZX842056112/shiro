def0af21f54a33f6db311b0961f383f12530af16
李宗效 <842056112@qq.com>
2020-10-29 21:35:56 +0800

    更新

---
+++ springboot_thymeleaf_shiro/src/main/java/com/baizhi/SpringbootThymeleafShiroApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafShiroApplication.class, args);
+    }
+
+}
